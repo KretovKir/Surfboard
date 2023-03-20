a69fb1a120b6f8cc91ee070ab3dfd93dee175b55
KretovKir <kretovkir526@mail.ru>
2023-03-21 00:33:03 +0300

    accord without closing

--- js/team.js
+++ js/team.js
@@ -1,5 +1,20 @@
+const openItem = item =>{
+    const container = item.closest('.team__item');
+    const contentBlock = container.find('.team__item-drop');
+    const textContent = contentBlock.find('.team__item-content');
+    const reqHeight = textContent.height();
+    
+    contentBlock.height(reqHeight);
+}
+
+const closeItem = container=>{
+    const items = container.find('.team__item-drop');
+    items.height(0);
+}
+
 $('.team__item-title').on('click', (e)=>{
-    e.preventDefault();
-    $('.team__item-drop').css('display', 'flex');
-    console.log('test')
+    const $this = $(e.currentTarget);
+    const container = $this.closest('.team')
+    closeItem(container);
+    openItem($this);
 })
