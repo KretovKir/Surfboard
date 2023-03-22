0c752760da3ae4851cba9cf2faf82b220db9fa1c
KretovKir <kretovkir526@mail.ru>
2023-03-23 00:19:32 +0300

    add accordeon

--- js/team.js
+++ js/team.js
@@ -3,18 +3,28 @@ const openItem = item =>{
     const contentBlock = container.find('.team__item-drop');
     const textContent = contentBlock.find('.team__item-content');
     const reqHeight = textContent.height();
-    
+    container.addClass('isOpen');
     contentBlock.height(reqHeight);
 }
 
 const closeItem = container=>{
     const items = container.find('.team__item-drop');
     items.height(0);
+    const itemContainer = container.find('.team__item')
+    itemContainer.removeClass('isOpen');
 }
 
 $('.team__item-title').on('click', (e)=>{
     const $this = $(e.currentTarget);
     const container = $this.closest('.team')
+    const elemContainer = $this.closest('.team__item');
+
+    if (elemContainer.hasClass('isOpen')){
+        closeItem(container);
+    }
+    else{
+        openItem(container);
+    }
     closeItem(container);
     openItem($this);
 })
