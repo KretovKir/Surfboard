789697b2a26f4feca23b9a88be8db526644e9d9c
KretovKir <kretovkir526@mail.ru>
2023-03-24 00:10:21 +0300

    full slideshow

--- js/reviews.js
+++ js/reviews.js
@@ -1,6 +1,15 @@
-$('.reviews__user').on('click', e =>{
-    e.preventDefault();
-    let currentTarget = e.currentTarget;
-    $(currentTarget).addClass('user-active').siblings().removeClass('user-active');
-})
+const findBlock = (alias)=>{
+   return $('.reviews__content').filter((ndx, item)=>{
+        return $(item).attr('data-open-with')==alias;
+    })
+}
 
+
+//я не понял к чему нужно было использовать reviews__switcher-link, если можно навесит
+$('.reviews__user').on('click', (event)=>{
+    let $this = $(event.currentTarget);
+    let target = $this.attr('data-open');
+    let showItem = findBlock(target);
+    $this.addClass('user-active').siblings().removeClass('user-active');
+    showItem.addClass('active').siblings().removeClass('active');
+})
