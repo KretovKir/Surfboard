8d411f73e97172d9b0270048eeea57e479190ef8
KretovKir <kretovkir526@mail.ru>
2023-03-31 19:45:10 +0300

    dev and prod

--- js/validation.js
+++ js/validation.js
@@ -4,32 +4,33 @@ sendBtn.addEventListener('click', (event)=>{
         console.log('ок')
         
         const form = $(event.currentTarget);
-    const name = form.find("[name = 'name']");
-    const phone = form.find("[name = 'phone']");
-    const street = form.find("[name = 'street']");
-    const house = form.find("[name = 'house']");
-    const corpus = form.find("[name = 'corpus']");
-    const apartment = form.find("[name = 'apartment']");
-    const floor = form.find("[name = 'floor']");
-    const to = form.find("[name='to']");
+        const name = form.find("[name = 'name']");
+        const phone = form.find("[name = 'phone']");
+        const street = form.find("[name = 'street']");
+        const house = form.find("[name = 'house']");
+        const corpus = form.find("[name = 'corpus']");
+        const apartment = form.find("[name = 'apartment']");
+        const floor = form.find("[name = 'floor']");
+        const to = form.find("[name='to']");
 
-    $.ajax({
-        url: "https://webdev-api.loftschool.com/sendmail",
-        method: "post",
-        data: {
-            name: name.val(),
-            phone: phone.val(),
-            street:street.val(),
-            house: house.val(),
-            corpus: corpus.val(),
-            apartment: apartment.val(),
-            floor: floor.val(),
-            to: to.val()
-        },
-        success: data => {
-            console.log(data);
-        }
-    })
+
+        $.ajax({
+            url: "https://webdev-api.loftschool.com/sendmail",
+            method: "post",
+            data: {
+                name: name.val(),
+                phone: phone.val(),
+                street:street.val(),
+                house: house.val(),
+                corpus: corpus.val(),
+                apartment: apartment.val(),
+                floor: floor.val(),
+                to: to.val()
+            },
+            success: data => {
+                console.log(data);
+            }
+        })
     }else{
         console.log('Ошибка');
         
