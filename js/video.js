a80bcd7b832b29e695521b17c80f1ef598841575
KretovKir <kretovkir526@mail.ru>
2023-04-06 23:29:33 +0300

    full player

--- js/video.js
+++ js/video.js
@@ -10,8 +10,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
   soundControl = document.getElementById("soundLevel");
   play = document.querySelectorAll(".play");
 
+  soundControl.addEventListener('click', changeSound);
+  soundControl.addEventListener('mouseup', changeSound);
+
   durationControl.value = 0;
   durationControl.min=0;
+
+  durationControl.addEventListener('mousedown', stopInterval);
+  durationControl.addEventListener('click', setVideoDuration);
 
     for (let i = 0; i<play.length; i++){
         play[i].addEventListener("click", ()=>{
@@ -23,7 +29,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
           if (video.paused) {
             video.play();
-            intervalId = setInterval(updateDuration,100/150)
+            intervalId = setInterval(updateDuration,1000/66)
           } else {
             video.pause();
             clearInterval(intervalId);
@@ -34,5 +40,41 @@ document.addEventListener("DOMContentLoaded", (event) => {
     function updateDuration(){
       durationControl.value = video.currentTime;
     } 
+
+    let micControl = document.getElementById('micLevel');
+    micControl.addEventListener('click', soundOff);
+
+    function soundOff(){
+      if(video.volume === 0){
+        video.volume = soundControl; 
+        soundControl.value = soundControl *10;
+      }
+      else{
+        soundLevel=video.volume;
+        video.volume = 0;
+        soundControl.value=0;
+      }
+    }
+
+    function stopInterval(){
+      video.pause();
+      clearInterval(intervalId);
+    }
+
+    function setVideoDuration(){
+      if(video.paused){
+        video.play();
+
+      }else{
+        video.pause();
+      }
+
+      video.currentTime = durationControl.value;
+      intervalId = setInterval(updateDuration, 1000/66);
+    }
+
+    function changeSound(){
+      video.volume = soundControl.value/10;
+    }
 
 });
