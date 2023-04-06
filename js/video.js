f2fd1f91db81f610a275a5e5588eb1815f740a01
KretovKir <kretovkir526@mail.ru>
2023-04-06 17:26:43 +0300

    start/paused and duration update

--- js/video.js
+++ js/video.js
@@ -8,13 +8,31 @@ document.addEventListener("DOMContentLoaded", (event) => {
   video = document.getElementById("video");
   durationControl = document.getElementById("durationLevel");
   soundControl = document.getElementById("soundLevel");
-  play = document.getElementById("play");
+  play = document.querySelectorAll(".play");
 
-  play.addEventListener("click", function(){
-    if (video.paused) {
-      video.play();
-    } else {
-      video.pause();
-    }
-  }, false);
+  durationControl.value = 0;
+  durationControl.min=0;
+
+    for (let i = 0; i<play.length; i++){
+        play[i].addEventListener("click", ()=>{
+
+          let playImg = document.querySelector('.play-btn');
+          playImg.classList.toggle('play-btn--active');
+
+          durationControl.max = video.duration;
+
+          if (video.paused) {
+            video.play();
+            intervalId = setInterval(updateDuration,100/150)
+          } else {
+            video.pause();
+            clearInterval(intervalId);
+          }
+        }
+    )}
+
+    function updateDuration(){
+      durationControl.value = video.currentTime;
+    } 
+
 });
