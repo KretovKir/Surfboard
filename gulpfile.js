af7ffb4613faccd955b657e091d64691de3e9d04
KretovKir <kretovkir526@mail.ru>
2023-04-09 13:39:36 +0300

    gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,48 @@
 const { src, dest, task, watch, series } = require("gulp");
 const rm = require("gulp-rm");
-const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
+const reload = browserSync.reload;
 
-sass.compiler = require('node-sass');
-const styles= [
-    "node_modules/normalize.css/normalize.css",
-    "scss/main.scss"
-]
-
+sass.compiler = require("node-sass");
+const styles = ["node_modules/normalize.css/normalize.css", "scss/main.scss"];
 
 task("cleanDir", function () {
   return src("dist/**/*", { read: false }).pipe(rm());
 });
 
 task("copy:html", () => {
-  return src("src/*.html").pipe(dest("dist"));
+  return src("src/*.html")
+    .pipe(dest("dist"))
+    .pipe(reload({ stream: true }));
 });
-task("copy:scss",()=>{
-    return src("src/scss/**/*.scss")
-    .pipe(dest('dist/scss'))
-})
 
-task("styles", ()=>{
-    return src('src/scss/**/*.scss')
-    .pipe(concat('main.scss'))
+task("copy:scss", () => {
+  return src("src/scss/**/*.scss")
     .pipe(dest("dist/css"))
-})
+});
 
+task("styles", () => {
+  return src("dist/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(concat("main.css"))
+    .pipe(dest("dist/css"))
+});
 
-task('server', () =>{
-    browserSync.init({
-        server: {
-            baseDir: "dist"
-        },
-        open: false
-    });
+task("server", () => {
+  browserSync.init({
+    server: {
+      baseDir: "dist",
+    },
+    open: false,
+  });
 });
 
 watch("scss/**/*.scss", series("styles"));
 watch("*.html", series("copy:html"));
 
-
-task("default", series("cleanDir", "copy:html", "copy:scss", "styles", "server"));
+task(
+  "default",
+  series("cleanDir", "copy:html", "copy:scss", "styles", "server")
+);
