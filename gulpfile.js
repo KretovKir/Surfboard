9d817e7ec85acec8c9eb9acba06cf1e2d23eaa25
KretovKir <kretovkir526@mail.ru>
2023-04-09 13:01:07 +0300

    gulp properties

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest, task, watch, series } = require("gulp");
 const rm = require("gulp-rm");
 const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 const styles= [
@@ -9,21 +10,37 @@ const styles= [
     "scss/main.scss"
 ]
 
+
 task("cleanDir", function () {
   return src("dist/**/*", { read: false }).pipe(rm());
 });
 
-task("copy", () => {
-  return src("scss/**/*.scss").pipe(dest("dist"));
+task("copy:html", () => {
+  return src("src/*.html").pipe(dest("dist"));
 });
-
-task("styles", ()=>{
-    return src('scss')
-    .pipe(concat('main.scss'))
-    .pipe(dest("scss"))
-    
+task("copy:scss",()=>{
+    return src("src/scss/**/*.scss")
+    .pipe(dest('dist/scss'))
 })
 
-watch("scss/**/*.scss", series("styles"));
+task("styles", ()=>{
+    return src('src/scss/**/*.scss')
+    .pipe(concat('main.scss'))
+    .pipe(dest("dist/css"))
+})
 
-task("default", series("cleanDir", "styles"));
+
+task('server', () =>{
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        },
+        open: false
+    });
+});
+
+watch("scss/**/*.scss", series("styles"));
+watch("*.html", series("copy:html"));
+
+
+task("default", series("cleanDir", "copy:html", "copy:scss", "styles", "server"));
